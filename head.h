e64ec95e8f56c366cd5103efbb4ea2c7a2191132
Andrey <liamo19@mail.ru>
2022-07-12 21:32:42 +0600

    Change head.h

--- head.h
+++ head.h
@@ -6,9 +6,16 @@
 #include <math.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <stdbool.h>
 
+void print_program(struct line* mini_string, int cnt_str, FILE *f_out);
+int cnt_str(int file_size, char* input_string);
+void construct_text(int file_size, struct line* mini_string, char* input_string);
+int comporator(const void * ptr1, const void * ptr2);
+int fileread(char * big_string, int file_size, FILE *f_in);
 size_t size_of_file(FILE *f);
-
+void original_sort(int file_size, char* input_string);
+int comporator_by_end(const void * ptr1, const void * ptr2);
 
 struct line
 {
