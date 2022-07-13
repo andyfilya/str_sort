be523ed845b1be40c544f93cfab3ac58ed244195
Andrey <liamo19@mail.ru>
2022-07-13 19:20:51 +0600

    Fixed problems

--- sort.cpp
+++ sort.cpp
@@ -12,11 +12,19 @@ size_t size_of_file(FILE *f)
     return PROGRAM_ERROR;
 }
 
-int fileread(char * big_string, int file_size, FILE *f_in)
+int fileread(data *text, FILE *f_in)
 {
     assert(f_in);
-    assert(big_string);
-    if (fread(big_string, sizeof(char), file_size, f_in) != file_size)
+    assert(text);
+
+    text->file_size = size_of_file(f_in);
+    text->input_string = (char*)calloc(text->file_size, sizeof(char));
+    if(!(text->input_string))
+    {
+        printf("ERROR: WE CAN'T ALLOCATE MEMORY\n");
+        return PROGRAM_ERROR;
+    }
+    if (fread(text->input_string, sizeof(char), text->file_size, f_in) != text->file_size)
     {
         printf("We can't read your file, sorry");
         return PROGRAM_ERROR;
@@ -65,72 +73,72 @@ int comporator_by_end(const void * ptr1, const void * ptr2)
     }
     return 0;
 }
-void construct_text(int file_size, struct line* mini_string, char* input_string)
+void construct_text(data *text)
 {
-    assert(mini_string && input_string);
+    assert(text->mini_string && text->input_string);
     bool flag = true;
     int begin_of_line = 0, order_of_line = 0;
 
-    for(int j = 0; j <= file_size; j++)
+    for(int j = 0; j <= text->file_size; j++)
     {
-        if((isalpha(input_string[j])) && (flag == true))
+        if((isalpha(text->input_string[j])) && (flag == true))
         {
-            mini_string[order_of_line].line_begin = &input_string[j];
+            text->mini_string[order_of_line].line_begin = &text->input_string[j];
             begin_of_line = j;
             flag = false;
         }
-        if(input_string[j] == '\0')
+        if(text->input_string[j] == '\0')
         {
-            mini_string[order_of_line].len_line = j - begin_of_line;
+            text->mini_string[order_of_line].len_line = j - begin_of_line;
             int end_of_line = j;
             flag = true;
-            while(isalpha(input_string[end_of_line]) == 0)
+            while(isalpha(text->input_string[end_of_line]) == 0)
             {
                 end_of_line -= 1;
-                mini_string[order_of_line].len_line -= 1;
+                text->mini_string[order_of_line].len_line -= 1;
             }
-            mini_string[order_of_line].len_line += 1;
+            text->mini_string[order_of_line].len_line += 1;
             order_of_line += 1;
         }
     }
 }
 
-int cnt_str(int file_size, char* input_string)
+int cnt_str(data *text)
 {
-    assert(input_string);
+    assert(text->input_string);
     int j = 0;
     int counter_of_strings = 0;
-    for(j = 0; j < file_size; j++)
+    for(j = 0; j < text->file_size; j++)
     {
-        if (input_string[j] == '\n')
+        if (text->input_string[j] == '\n')
         {
             counter_of_strings += 1;
-            input_string[j] = '\0';
+            text->input_string[j] = '\0';
         }
     }
-    input_string[j] = '\0';
+    text->input_string[j] = '\0';
     counter_of_strings  += 1;
     return counter_of_strings;
 }
 
-void print_program(struct line* mini_string, int cnt_str, FILE *f_out)
+void print_program(data *text, int cnt_str, FILE *f_out)
 {
     for (int j = 0; j < cnt_str; j++)
     {
-        fputs(mini_string[j].line_begin, f_out);
+        fputs(text->mini_string[j].line_begin, f_out);
         fputs("\n", f_out);
     }
     fputs("\n\n\n -------------------------- \n\n\n", f_out);
 }
-void original_sort(int file_size, char* input_string)
+void original_sort(data *text)
 {
     int i = 0;
-    for (i = 0; i < file_size; i++)
+    for (i = 0; i < text->file_size; i++)
     {
-        if (input_string[i] == '\0')
+        if (text->input_string[i] == '\0')
         {
-            input_string[i] = '\n';
+            text->input_string[i] = '\n';
         }
     }
-    input_string[i] = '\n';
+    text->input_string[i] = '\n';
 }
